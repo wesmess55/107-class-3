fa6c19585ea2da6b1f37ac1e587612777969cdd7
wesmess55 <66344929+wesmess55@users.noreply.github.com>
2020-08-04 22:42:48 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var server = express();
+
+var ejs = require('ejs');
+server.set('views',__dirname+ "/public");
+server.engine('html', ejs.renderFile);
+server.set('view engine',ejs);
+
+server.use(express.static(__dirname+ "/public"));
+
+
+var bparser = require("body-parser");
+server.use(bparser.json());
+
+server.get('/',function(req, res){
+    res.send("hello there!");
+});
+
+
+server.get('/about',function(req,res){
+    res.render('about.html');
+});
+
+server.get('/admin',function(req,res){
+    res.render("admin/admin.html");
+});
+
+server.get('/index',function(req,res){
+    res.render("index.html");
+});
+
+var data = [];
+
+
+server.post('api/items', function(req,res){
+    var item = req.body;
+    if(!item.price){
+        res.status(400);
+        res.send("Price Required for Item");
+    }
+    else{
+
+    Data.push(item);
+    item.id = cnt;
+    cnt+= 1;
+    
+    item.status = 'saved';
+    
+    res.status(201);
+    res.json(item);
+    }
+    
+
+});
+
+server.get('/test/1',(req,res)=>{
+
+    var nums = [81,3,1,543,-2,53,-28,897123,1,2,-9487745, 99];
+
+    function greater(){
+        for(i = 0; i < nums.length; i++){
+            if(i<nums[i])i=nums;
+        };
+        console.log(i);
+        return i;
+        
+    };
+    
+    res.render(i);
+
+})
+
+server.listen(8080, function(){
+    console.log("Server running at http://localhost:8080");
+});
+
